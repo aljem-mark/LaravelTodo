3437968160f2090038ebc465562a03d7bdf9f2c6
aljem-mark <speakout.aljem@gmail.com>
2019-09-17 23:51:07 +0800

    add auth, users seed and migration

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(User::class)->create([
+            'name' => 'Test User',
+            'email' => 'test@gmail.com'
+        ]);
+
+        factory(User::class, 10)->create();
+    }
+}
